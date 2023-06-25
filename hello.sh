78fff1c2cf9bda84ec6d27e4693531b984899944
Zheng Zhuoming <810404081@qq.com>
2023-06-25 17:29:17 +0800

    0625

--- hello.sh
+++ hello.sh
@@ -1,1 +1,6 @@
+
 echo "hello shell"
+hero="hero string"
+export shero=500
+export nobody="Mr Nobody"
+sh test1.sh 
---
+++ test1.sh
@@ -0,0 +1,6 @@
+echo $hero
+echo $shero
+echo ${#shero}
+echo ${hero:2:2}
+echo $nobody
+echo ${nobody:1:2}
